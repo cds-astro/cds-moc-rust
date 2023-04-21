783858a3bfc708d449158d5797db9524ed705c9f
fxpineau <francois-xavier.pineau@astro.unistra.fr>
2023-04-21 09:59:38 +0200

    Fix MOCWasm test

--- crates/wasm/tests/lib.rs
+++ crates/wasm/tests/lib.rs
@@ -1,7 +1,7 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
-use moc_wasm::*;
+use moc_wasm::smoc::MOC;
 use wasm_bindgen_test::*;
 
 use wasm_bindgen::JsValue;
@@ -9,7 +9,6 @@ use wasm_bindgen::JsValue;
 
 #[wasm_bindgen_test]
 fn cone() {
-  let name = "cone";  
-  from_cone(name, 4,  0.0, 0.0, 5.0).unwrap();
-  assert_eq!(to_ascii(name, None), JsValue::from_str("3/271 304 4/1128 1130-1131 1172-1173 1175 "));
+  let moc = MOC::from_cone(4,  0.0, 0.0, 5.0, None).unwrap();
+  assert_eq!(moc.to_ascii(None), Ok(JsValue::from_str("3/271 304 4/1128 1130-1131 1172-1173 1175 ")));
 }
